51b457be82a6bac9e35598219800f61c42cb024f
laurenball <laurenb@pvep.com>
2022-06-10 13:42:07 -0400

    Wipe it out

--- assets/js/geocities.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('geocities-toggle').addEventListener('click', partyTime);
-
-function partyTime() {
-  mainViewport = document.getElementById('page-top');
-  mainViewport.classList.toggle('geocities-mode');
-};
