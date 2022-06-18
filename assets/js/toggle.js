5d1f93a16decbcb71581a65ce61f481bd386421e
laurenball <laurenb@pvep.com>
2022-06-18 02:21:24 -0400

    Prettier

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,40 +1,43 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
   const selector = document.querySelector('.toggler input[type="checkbox"]');
 
   let savedTheme;
-  if (localStorage.getItem('theme')) {
-    savedTheme = localStorage.getItem('theme');
-  } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-    savedTheme = 'light'
+  if (localStorage.getItem("theme")) {
+    savedTheme = localStorage.getItem("theme");
+  } else if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    savedTheme = "light";
   } else {
-    savedtheme = 'dark'
+    savedtheme = "dark";
   }
 
   if (savedTheme) {
-      document.documentElement.setAttribute('data-theme', savedTheme);
+    document.documentElement.setAttribute("data-theme", savedTheme);
 
-      if (savedTheme === 'light') {
-          selector.checked = true;
-      }
+    if (savedTheme === "light") {
+      selector.checked = true;
+    }
   }
 
   function switchTheme(e) {
-    let newTheme = 'dark';
+    let newTheme = "dark";
     if (e.target.checked) {
-      newTheme = 'light';
+      newTheme = "light";
     }
 
-    document.documentElement.setAttribute('data-theme', newTheme);
-    localStorage.setItem('theme', newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
   }
 
-  selector.addEventListener('change', switchTheme);
+  selector.addEventListener("change", switchTheme);
 
-  const label = document.querySelector('label');
-  label.addEventListener('keydown', event => {
-    if (event.key === 'Enter'){
-      selector.checked = !selector.checked
-      selector.dispatchEvent(new Event('change'));
+  const label = document.querySelector("label");
+  label.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      selector.checked = !selector.checked;
+      selector.dispatchEvent(new Event("change"));
     }
   });
-})
+});
