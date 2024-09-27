f42bd324ba087c328eb285689be8e98e89aff90e
cam-ball <hi@camball.me>
2024-09-27 18:36:09 -0400

    view height

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline bg-slate text-purple">
+    <div className="h-full text-3xl font-bold underline bg-slate text-purple">
       hello world
     </div>
   )
