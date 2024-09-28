cf1ed36965c89d2b2aeb2bca420d2b71b3e5e0c9
cam-ball <hi@camball.me>
2024-09-28 12:44:17 -0400

    Recreate about page

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -1,18 +1,35 @@
-const CenterCard = () => {
+import type { ReactNode } from "react";
+
+const CenterCard = ({
+  pageTitle,
+  children,
+}: {
+  pageTitle: string;
+  children: ReactNode;
+}) => {
+  const formattedTitle = `$ ${pageTitle}`;
   return (
-    <div className="h-full flex items-center justify-center text-white">
-      <section className="border-inset border-8 border-purple max-w-[1200px] w-[60vh] mx-12 p-10">
-        <div className="border-0 border-b-2 border-purple border-solid p-3 mb-3">
-          <h1 className="text-6xl text-red font-mono font-semibold mb-3">
-            Cameron Ball
-          </h1>
-          <h2 className="text-2xl italic text-right">
-            Senior <span className="text-red">Ruby</span> Engineer
-          </h2>
+    <>
+      <title>{formattedTitle}</title>
+      <div className="h-full flex items-center justify-center text-white">
+        <div className="border-inset border-8 border-purple max-w-[1200px] min-w-[650px] mx-12 p-10">
+          <div className="border-0 border-b-2 border-purple border-solid p-3 mb-3">
+            <h1 className="text-5xl text-red font-mono font-semibold mb-3">
+              Cameron Ball
+            </h1>
+            <h2 className="text-2xl italic text-right">
+              Senior <span className="text-red">Ruby</span> Engineer
+            </h2>
+          </div>
+          <section>
+            <h3 className="font-semibold text-2xl font-mono my-6">
+              {formattedTitle}
+            </h3>
+            {children}
+          </section>
         </div>
-        something
-      </section>
-    </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,26 @@
 import CenterCard from "../components/CenterCard";
 
 const About = () => {
+  const startYear = 2015;
+  const yearsExperience = new Date().getFullYear() - startYear;
+
   return (
     <div className="h-full">
-      <CenterCard />
+      <CenterCard pageTitle="whoami">
+        <ul className="list-[square] list-inside text-lg ml-2">
+          <li>Boston, MA</li>
+          <li>{yearsExperience} years of professional experience</li>
+          <li>
+            Brought to you by the letter "R":{" "}
+            <ul className="list-[square] list-inside ml-12">
+              <li>Ruby</li>
+              <li>Rails</li>
+              <li>React</li>
+            </ul>
+          </li>
+          <li>Afraid of heights</li>
+        </ul>
+      </CenterCard>
     </div>
   );
 };
