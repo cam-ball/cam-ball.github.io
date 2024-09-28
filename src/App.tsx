4f19d2a0d7430d5bca2c2dbf47a7ad839610206d
cam-ball <hi@camball.me>
2024-09-28 13:37:54 -0400

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 // import { useState } from 'react'
 import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Hobbies from "./pages/Hobbies";
+import Contact from "./pages/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <About />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/hobbies",
+    element: <Hobbies />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 function App() {
   return (
     <div className="h-full bg-slate">
-      <Projects />
+      <RouterProvider router={router} />
     </div>
   );
 }
