fe018bd2818b2f1917089a2636cc446e43c2a042
cam-ball <hi@camball.me>
2025-04-08 19:47:52 -0400

    recreate nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,27 +7,30 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
-  const listStyles = "border w-20 h-20 p-4 bg-purple text-center";
+  const hoverStyles =
+    "hover:text-red hover:bg-inherit hover:border-red hover:translate-y-2 transition ease-in-out";
+  const listStyles = `border border-4 border-purple w-20 h-20 bg-purple text-center ${hoverStyles}`;
+  const linkStyles = "w-full h-full flex items-center justify-center";
 
   return (
     <ul className="w-full flex justify-between text-white px-6 text-4xl">
       <li className={listStyles}>
-        <a href="/" className="w-full h-full">
+        <a href="/" className={linkStyles}>
           <FontAwesomeIcon icon={faPersonRays} />
         </a>
       </li>
       <li className={listStyles}>
-        <a href="/projects">
+        <a href="/projects" className={linkStyles}>
           <FontAwesomeIcon icon={faLaptopCode} />
         </a>
       </li>
       <li className={listStyles}>
-        <a href="/hobbies">
+        <a href="/hobbies" className={linkStyles}>
           <FontAwesomeIcon icon={faDiceD20} />
         </a>
       </li>
       <li className={listStyles}>
-        <a href="/contact">
+        <a href="/contact" className={linkStyles}>
           <FontAwesomeIcon icon={faComments} />
         </a>
       </li>
